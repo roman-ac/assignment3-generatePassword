4f1444476397d723a0202a2126c1b35bfa1f8489
Roman Ac <roman25xx@gmail.com>
2021-12-12 14:54:57 +1100

    completed but not working

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var characters = ["!","@","#","$","%","&","?","+","~"];
+var chosenCharacters = [""]; // empty string
 
 // we are going to start with creating the missing generatePassword function
 
 function generatePassword () {
+
+  var result ="";
 
 //  ask for password length 
 var passwordLength = window.prompt ("Please choose a password length of between 8 - 128.");
@@ -39,6 +42,30 @@ if (!askUppercase && !askLowercase && !askNumbers && !askCharacters) {
   window.alert ("Please select at least one special character type!");
   return generatePassword();
 }
+
+if (askUppercase = true) {
+  chosenCharacters += uppercase; // googled the value of +=
+}
+
+if (askLowercase = true) {
+  chosenCharacters += lowercase;
+}
+
+if (askNumbers = true) {
+  chosenCharacters += numbers;
+}
+
+if (askCharacters = true) {
+  chosenCharacters += characters;
+}
+
+var result = Math.floor(Math.random() * chosenCharacters.passwordLength);
+
+//var result = chosenCharacters[index];
+
+return result;
+
+
 
 
 }
