5f78bec7c55621c03b626e915cde6f0050f5ec22
Roman Ac <roman25xx@gmail.com>
2021-12-12 11:33:46 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# <Week 3 Assignment>
+
+# <Project GeneratePassword>
+
+## This assignment is an opportunity to apply and demonstrate the learning from past 3 classes on JS. 
+
+# Mission
+    * to create a password within the given parameters.
+
+
+# Parameters
+    * password length to be between 8 - 128 character
+    * ask user whether or not use uppercase, lowercase, numbers or special character
+    * then validate at least one character type is selected
+    * when all answered; generate password that matches the selected criteria
---
+++ script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// we are going to start with creating the missing generatePassword function
+
+function generatePassword () {
+
+//  ask for password length 
+var passwordLength = window.prompt ("Please choose a password length of between 8 - 128.");
+
+// verify the input is correct 
+if (isNaN(passwordLength)) {
+  window.alert ("Must be a numeric value!");
+  return generatePassword();
+}
+
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
